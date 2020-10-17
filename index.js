c96fe70b7d053c4b809af75e97a77eb55249913e
hi-artem <aakatev@virtru.com>
2020-10-17 16:59:47 -0700

    Added simple app

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const terraform = require('./src/terraform')
 
 const main = async () => {
-  let result = await terraform.version()
-  console.log(result)
+  let resultInit = await terraform.init()
+  console.log(resultInit)
+  if(resultInit.stderr) {
+    return
+  }
+
+  let resultApply = await terraform.apply()
+  console.log(resultApply)
+  if(resultApply.stderr) {
+    return
+  }
+  let resultDestroy = await terraform.destroy()
+  console.log(resultDestroy)
+  if(resultApply.stderr) {
+    return
+  }
 }
 
 main()
--- src/terraform.js
+++ src/terraform.js
@@ -1,15 +1,17 @@
-const tf = process.env.TF_PATH || '/usr/local/bin/terraform'
+const tf = process.env.TF_PATH || 'TF_IN_AUTOMATION=true /usr/local/bin/terraform'
 
 const execute = require('./exec')
 
 const version = () => execute(`${tf} version`)
 const init = () => execute(`${tf} init`)
 const plan = () => execute(`${tf} plan`)
-const apply = () => execute(`${tf} apply`)
+const apply = () => execute(`${tf} apply --auto-approve`)
+const destroy = () => execute(`${tf} destroy --auto-approve`)
 
 module.exports = {
   version,
   init,
   plan,
-  apply
+  apply,
+  destroy,
 }
